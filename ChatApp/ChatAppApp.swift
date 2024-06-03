c0384e07beef7d85ae17c3f8faa34574e4243a55
rujin <rujindevkota@gmail.com>
2024-06-03 13:44:38 +0545

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Apple on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
